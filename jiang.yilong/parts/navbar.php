800aed791af8607e94ce1726f620ceb96e36cd98
yilongj <yilongj@yilongj.com>
2021-10-24 18:54:37 -0700

    first commit

---
+++ jiang.yilong/parts/navbar.php
@@ -0,0 +1,20 @@
+<header class="navbar">	
+        <div class="container display-flex">	
+        	<div class="flex-none">	
+               <h1>	store</h1>
+        	</div>
+        	<div class="flex-stretch">	</div>
+        	<nav class="nav nav-flex flex-none">	
+
+               <ul>	
+                  
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="product_list.php">link 2</a></li>
+                  <li><a href="#">link 3</a></li>	
+               </ul>
+        	</nav>
+
+
+        </div>
+
+</header>
