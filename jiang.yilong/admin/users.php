6d896eb64d687ea40303bc26fe03a5fe8d011a29
yilongj <yilongj@yilongj.com>
2021-11-08 00:46:15 -0800

    first commit

--- jiang.yilong/admin/users.php
+++ jiang.yilong/admin/users.php
@@ -5,17 +5,49 @@ include "../lib/php/functions.php";
 
 
 
+$filename = "../data/users.json";
+$users = file_get_json($filename);
 
-$users = file_get_json("../data/users.json");
+
+// file_put_contents json_encode explode $_post
+// CRUD, Create Read Update Delete
 
 
-$classes = implode(", ", $user->classes);
+
+// print_p([$_GET,$_post]);
+
+if(isset($_GET['action'])) {
+   switch($_GET['action']) {
+         case "update":
+             $users[$_GET['id']]->name = $_POST['user-name'];
+             $users[$_GET['id']]->type = $_POST['user-type'];
+             $users[$_GET['id']]->email = $_POST['user-email'];
+             $users[$_GET['id']]->classes = explode(", ", $_POST['user-classes']);
+
+
+             file_put_contents($filename,json_encode($users));
+             header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
+             break;
+         case "create":
+             break;
+
+         case "delete":
+             break;
+   }
+
+}
+
+
+
 
 
 
 
 
 function showUserPage($user) {
+
+$id = $_GET['id']
+$classes = implode(", ", $user->classes);
 // heredoc
 echo <<<HTML
 <nav class="nav nav-crumbs">
@@ -23,23 +55,34 @@ echo <<<HTML
       <li><a href="admin/users.php">Back</a></li>
    </ul>
 </nav>
-<div>
-   <h2>$user->name</h2>
-   <div>
-      <strong>Type</strong>
-      <span>$user->type</span>
+<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=update">
+   <div class="form-control">
+      <label class="form-label" for="user-name">Name</label>
+      <input class="form-input" name="user-name" id="user-name" type="text" value="$user->name" placeholder="
+      Enter The User Name">
    </div>
 
-   <div>
-      <strong>Email</strong>
-      <span>$user->email</span>
+   <div class="form-control">
+      <label class="form-label" for="user-type">Type</label>
+      <input class="form-input" name="user-type" id="user-type" type="text" value="$user->type" placeholder="
+      Enter The User Type">
    </div>
 
-   <div>
-      <strong>Classes</strong>
-      <span>$classes</span>
+   <div class="form-control">
+      <label class="form-label" for="user-email">Email</label>
+      <input class="form-input" name="user-email" id="user-email" type="text" value="$user->email" placeholder="
+      Enter The User Email">
    </div>
-</div>
+
+   <div class="form-control">
+      <label class="form-label" for="user-classes">Classes</label>
+      <input class="form-input" name="user-classes" id="user-classes" type="text" value="$classes" placeholder="
+      Enter The User Classes, comma separated">
+   </div>
+   <div class="form-control">
+      <input class="form-button" type="submit" value="Save Changes">
+   </div>
+</form>
 HTML;
 
 }
