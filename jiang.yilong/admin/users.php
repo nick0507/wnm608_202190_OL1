1b8187693fab87810c98656fc8b06392988a7719
yilongj <yilongj@yilongj.com>
2021-11-15 02:00:31 -0800

    first commit

--- jiang.yilong/admin/users.php
+++ jiang.yilong/admin/users.php
@@ -9,6 +9,17 @@ $filename = "../data/users.json";
 $users = file_get_json($filename);
 
 
+$empty_user = (object)[
+   "name"=>"",
+   "type"=>"",
+   "email"=>"",
+   "classes"=>[]
+
+
+
+];
+
+
 // file_put_contents json_encode explode $_post
 // CRUD, Create Read Update Delete
 
@@ -29,9 +40,25 @@ if(isset($_GET['action'])) {
              header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
              break;
          case "create":
+             $empty_user->name = $_POST['user-name'];
+             $empty_user->type = $_POST['user-type'];
+             $empty_user->email = $_POST['user-email'];
+             $empty_user->classes = explode(", ", $_POST['user-classes']);
+
+
+             $id = count($users);
+
+             $users[] = $empty_user;
+
+
+             file_put_contents($filename,json_encode($users));
+             header("location:{$_SERVER['PHP_SELF']}?id=$id");
              break;
 
          case "delete":
+             array_splice($users,$_GET['id'],1);
+             file_put_contents($filename,json_encode($users));
+             header("location:{$_SERVER['PHP_SELF']}");
              break;
    }
 
@@ -46,16 +73,32 @@ if(isset($_GET['action'])) {
 
 function showUserPage($user) {
 
-$id = $_GET['id']
+$id = $_GET['id']；
+$addoredit = $id == "new" ? "Add" : "Edit";
+$createorupdate = $id == "new" ? "create" : "update";
 $classes = implode(", ", $user->classes);
 // heredoc
-echo <<<HTML
-<nav class="nav nav-crumbs">
-   <ul>
-      <li><a href="admin/users.php">Back</a></li>
-   </ul>
-</nav>
-<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=update">
+$display = <<<HTML
+<div>
+   <h2>$user->name</h2>
+   <div>
+      <strong>Type</strong>
+      <span>$user->type</span>
+   </div>
+   <div>
+      <strong>Email</strong>
+      <span>$user->email</span>
+   </div>
+   <div>
+      <strong>Classes</strong>
+      <span>$classes</span>
+   </div>
+</div>
+HTML;
+
+$form = <<<HTML
+<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=$createorupdate">
+   <h2>$addoredit User</h2>
    <div class="form-control">
       <label class="form-label" for="user-name">Name</label>
       <input class="form-input" name="user-name" id="user-name" type="text" value="$user->name" placeholder="
@@ -85,6 +128,27 @@ echo <<<HTML
 </form>
 HTML;
 
+$output = $id == "new" ? $form :
+    "<div class='grid gap'>
+
+       <div class='col-xs-12 col-md-7'>$display</div>
+       <div class='col-xs-12 col-md-5'>$form</div>
+    </div>
+    ";
+
+
+$delete = $id == "new" ? "" : "<a href='{$_SERVER['PHP_SELF']}?id=$id&action=delete'>Delete</a>";
+
+echo <<<HTML
+
+<nav class="display-flex">
+      <div class="flex-stretch"><a href="{$_SERVER['PHP_SELF']}">Back</a></div>
+      <div class="flex-none">$delete</div>
+</nav>
+$output
+
+
+HTML;
 }
 
 
@@ -109,7 +173,8 @@ HTML;
           <div class="flex-stretch"></div>
           <nav class="nav nav-flex flex-none">
              <ul>
-                <li><a href="admin/users.php">User List</a></li>
+                <li><a href="<?= $_SERVER['PHP_SELF'] ?>">User List</a></li>
+                <li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New User</a></li>
              </ul>
           </nav>
        </div>
@@ -126,7 +191,7 @@ HTML;
             <?php 
 
             if(isset($_GET['id'])) {
-                 showUserPage($users[$_GET['id']]);
+                 showUserPage($_GET['id'] == "new" ? $empty_user : $users[$_GET['id']]);
             } else {
 
             
